d260afbccc449b652eaae026dab9804dbb808b93
mory <papyrusabdallah@gmail.com>
2022-05-28 15:04:21 +0000

    add backent features

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,5 @@
 import express from "express";
-//import path from "path";
+import path from "path";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
 
@@ -18,6 +18,16 @@ const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+const __dirname = path.resolve();
+app.use(express.static(path.join(__dirname, "/frontend/build")));
+app.get("*", (req, res) =>
+  res.sendFile(path.join(__dirname, "/frontend/build/index.html"))
+);
+
+app.use((err, req, res, next) => {
+  res.status(500).send({ message: err.message });
+});
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => {
